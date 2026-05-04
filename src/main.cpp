aa72cc114fbfc7638d6df2939a7f79d018bff8a1
Jelmer Snippe <jelmersnippe@hotmail.com>
2026-05-04 20:21:40 +0200

    Load level by index

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,7 @@
 #include "raylib.h"
 #include "scenes/level_scene.hpp"
 
+#include <algorithm>
 #include <vector>
 
 void Update(GameState& state) {
@@ -34,8 +35,11 @@ void HandleInput(GameState& state) {
 int main() {
     std::vector<Level> levels = LoadLevels();
 
+    const int index = 0;
+    auto it = std::ranges::find_if(levels, [](Level& level) { return level.index == index; });
+
     GameState state;
-    LoadLevel(levels[0], state);
+    LoadLevel(*it, state);
 
     InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Sokoban");
 
